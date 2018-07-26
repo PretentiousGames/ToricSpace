c2698ad000f9d584ea5b6b47e5ecacd2b75fc3e9
theTrueMikeBrown <michael.brown@coxautoinc.com>
2018-07-26 11:05:37 -0600

    Sane names activate!

--- toric-space/src/domain/itemDrawer.js
+++ toric-space/src/domain/itemDrawer.js
@@ -2,28 +2,20 @@ class ItemDrawer {
     draw(item, viewPort, gameHeight, gameWidth) {
         var angleInRadians = item.a * (Math.PI / 180);
         var biggestSize = Math.max(item.imageWidth, item.imageHeight);
-        var iX = item.x;
-        var iY = item.y;
-        var vpX = viewPort.x;
-        var vpY = viewPort.y;
-        var vpW = viewPort.width;
-        var vpH = viewPort.height;
+        var viewPortLeft = viewPort.x - biggestSize;
+        var viewPortRight = viewPort.x + viewPort.width + biggestSize;
+        var viewPortTop = viewPort.y - biggestSize;
+        var viewPortBottom = viewPort.y + viewPort.height + biggestSize;
 
-        var vpL = vpX - biggestSize;
-        var vpR = vpX + vpW + biggestSize;
-        var vpT = vpY - biggestSize;
-        var vpB = vpY + vpH + biggestSize;
+        while (item.x < viewPortLeft) { item.x += gameWidth; }
+        while (item.x > viewPortRight) { item.x -= gameWidth; }
+        while (item.y < viewPortTop) { item.y += gameHeight; }
+        while (item.y > viewPortBottom) { item.y -= gameHeight; }
 
-        while (iX < vpL) { iX += gameWidth; }
-        while (iX > vpR) { iX -= gameWidth; }
-        while (iY < vpT) { iY += gameHeight; }
-        while (iY > vpB) { iY -= gameHeight; }
-
-        if (iX > vpL && iX < vpR &&
-            iY > vpT && iY < vpB) {
-            // visible;
-            var xt = (iX - vpX);
-            var yt = (iY - vpY);
+        if (item.x > viewPortLeft && item.x < viewPortRight &&
+            item.y > viewPortTop && item.y < viewPortBottom) {
+            var xt = (item.x - viewPort.x);
+            var yt = (item.y - viewPort.y);
             viewPort.context.translate(xt, yt);
             viewPort.context.rotate(angleInRadians);
             viewPort.context.drawImage(item.image,
@@ -34,8 +26,6 @@ class ItemDrawer {
             viewPort.context.rotate(-angleInRadians);
             viewPort.context.translate(-xt, -yt);
         }
-        //        var x = ((iX % iWidth) - iWidth) % iWidth;
-        //        var y = ((-viewPort.y % iHeight) - iHeight) % iHeight;
     }
 }
 
